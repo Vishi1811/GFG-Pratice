a58e9ca6a7df30cf7edf9fe30f2aac149d067fa5
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-03-29 10:39:48 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Plus One/plus-one.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    vector<int> increment(vector<int> arr ,int N) {
+        // code here
+        vector<int>ans;
+         int carry=1;
+        for(int i=N-1;i>=0;i--)
+        {
+
+            ans.push_back((carry+arr[i])%10);
+            carry=(carry+arr[i])/10;
+        }
+        if(carry)
+            ans.push_back(carry);
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        char c;
+        
+        cin>>N;
+        vector<int> arr(N);
+        
+        for(int i=0 ; i<N ; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        vector<int> res = ob.increment(arr,N);
+        for(int i: res)
+            cout<<i<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
