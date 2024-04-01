eca5ab0bd81210ffd45288ee982295a7d34f3dbf
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-04-01 08:49:19 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Prime Number/prime-number.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int isPrime(int N){
+        // code here
+        if (N <= 1) {
+            return false;
+        }
+        if (N <= 3) {
+            return true;
+        }
+        if (N % 2 == 0 || N % 3 == 0) {
+            return false;
+        }
+        int i = 5;
+        while (i * i <= N) {
+            if (N % i == 0 || N % (i + 2) == 0) {
+                return false;
+            }
+            i += 6;
+        }
+        return true;
+       
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.isPrime(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
