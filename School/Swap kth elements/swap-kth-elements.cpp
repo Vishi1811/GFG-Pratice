54b667220ed9764f7049bf2f24203427692c037a
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-04-02 12:18:09 +0530

    Added Solution - GfG to GitHub

---
+++ School/Swap kth elements/swap-kth-elements.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void swapKth(int n, int k, vector<int> &arr) {
+        // code here
+        int temp=arr[k-1];
+        arr[k-1]=arr[n-k];
+        arr[n-k]=temp;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        int k;
+        scanf("%d",&k);
+        
+        
+        vector<int> arr(n);
+        Array::input(arr,n);
+        
+        Solution obj;
+        
+        obj.swapKth(n, k, arr);
+        Array::print(arr);
+        
+    }
+}
+
+// } Driver Code Ends
