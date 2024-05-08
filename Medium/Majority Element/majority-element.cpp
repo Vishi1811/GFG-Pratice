32c5e37d9ce40356cdfb24a4384da5640a63c1fd
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-05-08 09:51:02 +0530

    Updated Solution - GfG to GitHub

--- Medium/Majority Element/majority-element.cpp
+++ Medium/Majority Element/majority-element.cpp
@@ -16,32 +16,35 @@ class Solution{
     int majorityElement(int a[], int size)
     {
         
-        int c=1,r=0;
-        for(int i=1;i<size;i++)
-        {
-            if(a[r]==a[i])
-            {
-                c++;
-            }
-            else
-                c--;
-            if(c==0)
-            {
-                c=1;
-                r=i;
-            }
-        }
-        c=0;
-        for(int i=0;i<size;i++)
-        {
-            if(a[r]==a[i])
-                c++;
-        }
-        if(c>(size/2))
-            return a[r];
-        return -1;
-        
+        // your code here
+         int count=0,m=a[0];
+         for(int i=0;i<size;i++)
+         {
+             if(a[i]==m)
+             {
+                 count++;
+             }
+             else
+             {
+                 count--;
+             }
+             if(count==0)
+             {
+                 m=a[i];
+                 count=1;
+             }
+         }
+         count=0;
+         for(int i=0;i<size;i++)
+         {
+             if(a[i]==m)
+             {
+                 count++;
+             }
+         }
+         return (count>size/2)?m:-1;
     }
+
 };
 
 //{ Driver Code Starts.
