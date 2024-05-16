58efd9efe69587912059393d21f2ab6061f61a30
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-05-16 13:03:05 +0530

    Updated Solution - GfG to GitHub

--- Easy/Second Largest/second-largest.cpp
+++ Easy/Second Largest/second-largest.cpp
@@ -11,20 +11,20 @@ public:
 	// largest elements
 	int print2largest(int arr[], int n) {
 	    // code here
-	    int largest=arr[0];
-	    int second_largest=-1;
-	    for(int i=1;i<n;i++)
-	    {
-	        if(arr[i]>largest)
-	        {
-	            second_largest=largest;
-	            largest=arr[i];
-	        }
-	        else if(arr[i] < largest && arr[i] > second_largest) {
-                second_largest = arr[i];
-	        }
-	    }
-	    return second_largest;
+	   int l=arr[0],sl=-1;
+	   for(int i=0;i<n;i++)
+	   {
+	       if(arr[i]>l)
+	       {
+	           sl=l;
+	           l=arr[i];
+	       }
+	       if(arr[i]>sl && arr[i]<l)
+	       {
+	           sl=arr[i];
+	       }
+	   }
+	   return sl;
 	}
 };
 
