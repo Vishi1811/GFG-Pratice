20ce32b122a3f1441a41e00b275f05a91e74a806
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-09-11 09:29:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Cost of ropes/minimum-cost-of-ropes.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return the minimum cost of connecting the ropes.
+    long long minCost(vector<long long>& arr) {
+        // Your code here
+        priority_queue<long long,vector<long long>,greater<long long>>q;
+        long long ans=0;
+        for(auto it:arr)q.push(it);
+        while(q.size()>1){
+            long long a=q.top();
+            q.pop();
+            a+=q.top();
+            q.pop();
+            ans+=a;
+            q.push(a);
+        }
+        return ans;
+    }
+    
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    long long t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string input;
+        long long num;
+        vector<long long> a;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+        Solution ob;
+        cout << ob.minCost(a) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
