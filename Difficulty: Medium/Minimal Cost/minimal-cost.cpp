7ce4b09d6bd6bab5e86a4e4a782ff89b0a696487
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-09-28 13:42:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimal Cost/minimal-cost.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int minimizeCost(int k, vector<int>& arr) {
+       int n=arr.size();
+        vector<int>dp(n,INT_MAX);
+        dp[0]=0;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=k && i+j<n;j++){
+                dp[i+j]=min(dp[i+j],dp[i]+abs(arr[i]-arr[i+j]));
+            }
+        }
+        return dp[n-1];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.minimizeCost(k, arr);
+        cout << res << endl;
+        // string tl;
+        // getline(cin, tl);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
