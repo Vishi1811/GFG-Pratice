e40a4d1dea9e55f6efa9141b0dee065b03040403
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-10-08 09:18:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bit Manipulation/bit-manipulation.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void bitManipulation(int num, int i) {
+        // your code here
+       //
+        
+        cout << ((num >> i-1) & 1) << " "
+        << (num | (1 << i-1)) << " "
+        << (num & (~(1 << i-1)));    // Check ith bit
+          
+           
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n >> i;
+        Solution ob;
+        ob.bitManipulation(n, i);
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
