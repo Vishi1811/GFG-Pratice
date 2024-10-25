6845b0f9eac082df29371293e23b80f834729b88
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-10-25 07:36:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Alternative Sorting/alternative-sorting.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    vector<int> alternateSort(vector<int>& arr) {
+        vector<int>temp;
+        temp=arr;
+        sort(temp.begin(),temp.end());
+        int i=0,j=arr.size()-1,k=0,n=arr.size();
+        while(k<=n)
+        {
+            if(k%2==0)arr[k++]=temp[j--];
+            
+            else arr[k++]=temp[i++];
+            
+        }
+        return arr;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.alternateSort(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
