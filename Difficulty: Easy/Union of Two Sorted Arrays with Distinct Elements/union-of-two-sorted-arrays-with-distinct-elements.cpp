a7a0f0f3e914c50e965bde906214b481f2306b7c
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-11-11 15:28:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Union of Two Sorted Arrays with Distinct Elements/union-of-two-sorted-arrays-with-distinct-elements.cpp
+++ Difficulty: Easy/Union of Two Sorted Arrays with Distinct Elements/union-of-two-sorted-arrays-with-distinct-elements.cpp
@@ -17,6 +17,16 @@ class Solution {
         int i=0,j=0 ;
         while(i<n && j<m)
         {
+             if(i>0&&a[i]==a[i-1])
+            {
+                i++;
+                continue;
+            }
+            if(j>0 && b[j]==b[j-1])
+            {
+                j++;
+                continue;
+            }
             if(a[i]==b[j])
             {
                 i++;
