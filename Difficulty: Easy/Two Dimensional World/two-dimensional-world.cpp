4bc1cbb4ce75ee7dd2dbdc9f94adb13b38352bcb
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2024-12-07 16:58:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Dimensional World/two-dimensional-world.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/* Function to take input for 2D array elements
+ * Note : Mention matrix in argument also.
+ * N : size of matrix
+ */
+void twoDimensional(/*Your Parameter here*/ int**arr, int N) {
+        for(int i=0;i<N;i++)
+        {
+            for(int j=0;j<N;j++)
+            {
+                cout<<arr[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+
+    // Loop to iterate through matrix
+    // and print all the matrix elements row wise
+}
+
+//{ Driver Code Starts.
+
+// Driver code
+int main() {
+
+    int testcase;
+    cin >> testcase;
+
+    while (testcase--) {
+        int N;
+        cin >> N;
+
+        // Declaring mat as pointer to pointer
+        int **mat;
+
+        // mat pointer contains array of pointer to array
+        mat = new int *[N];
+
+        // Taking input to mat[][]
+        for (int i = 0; i < N; i++) {
+            mat[i] = new int[N];
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                cin >> mat[i][j];
+            }
+        }
+
+        twoDimensional(mat, N);
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
