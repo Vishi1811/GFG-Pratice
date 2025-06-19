6120a25df8d0b2f6c94b54865523d78975b46115
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2025-06-19 08:13:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
+++ Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
@@ -1,29 +1,39 @@
 class Solution {
+    private:
+    int noOfGasRequired(vector<int>&arr,long double dist,int n)
+    {
+        int cnt=0;
+        for(int i=0;i<n-1;i++)
+        {
+            int gasInBetween=(arr[i+1]-arr[i])/dist;
+            if(((arr[i+1]-arr[i])/dist)==gasInBetween*dist)gasInBetween--;
+            cnt+=gasInBetween;
+        }
+        return cnt;
+    }
+    long double maxDiff(vector<int>&arr,int n)
+    {
+        long double ans = 0;
+        for (int i = 0; i < n - 1; i++) {
+            ans = max(ans, (long double)(arr[i + 1] - arr[i]));
+        }
+        return ans;
+    }
   public:
     double findSmallestMaxDist(vector<int> &arr, int k) {
         // Code here
         int n = arr.size();
-        // Min-heap with max gap after split = total gap / (number of parts)
-        priority_queue<pair<long double, int>> pq;
-        vector<int> howMany(n - 1, 0);
-
-        for (int i = 0; i < n - 1; i++) {
-            long double dist = arr[i + 1] - arr[i];
-            pq.push({dist, i});
+        long double low=0,high=maxDiff(arr,n);
+        long double diff=1e-6;
+        while(high-low > diff )
+        {
+            long double mid =(low+high)/2.0;
+            int cnt=noOfGasRequired(arr,mid,n);
+            if(cnt>k)low=mid;
+            else high=mid;
         }
-
-        for (int i = 0; i < k; i++) {
-            auto top = pq.top(); pq.pop();
-            int idx = top.second;
-            howMany[idx]++;
-            long double newDist = (arr[idx + 1] - arr[idx]) / (long double)(howMany[idx] + 1);
-            pq.push({newDist, idx});
-        }
-
-       long double ans = pq.top().first;
-
-        // Round to 2 decimal places before returning
-        return round(ans * 100.0) / 100.0;
+        return round(high * 100.0) / 100.0;
+      
     
     }
 };
